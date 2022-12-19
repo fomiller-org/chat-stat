ab26bb2ef770abc8a84639d5cf74294a6509ba7a
Forrest Miller <forrestmillerj@gmail.com>
2022-12-19 17:30:10 -0600

    anonymous bot connected

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/fomiller/chat-stat
+
+go 1.18
+
+require (
+	github.com/gempir/go-twitch-irc/v3 v3.2.0
+	github.com/joho/godotenv v1.4.0
+	golang.org/x/oauth2 v0.3.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.3.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
 
-func main()  {
-   fmt.Println("hello")
+	"github.com/gempir/go-twitch-irc/v3"
+)
+
+func main() {
+	client := twitch.NewAnonymousClient() // for an anonymous user (no write capabilities)
+
+	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
+		log.Println(message.Message)
+	})
+
+	client.Join("sodapoppin")
+
+	err := client.Connect()
+	if err != nil {
+		panic(err)
+	}
 }
