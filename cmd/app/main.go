3e4e9c70fa5ef9ba228872dccd45156c8e238fd1
Forrest Miller <forrestmillerj@gmail.com>
2022-12-22 21:36:33 -0600

    timeseries

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"strings"
 
+	redisTS "github.com/RedisTimeSeries/redistimeseries-go"
 	"github.com/gempir/go-twitch-irc/v3"
-	"github.com/go-redis/redis/v9"
-	"github.com/google/uuid"
 )
 
 var ctx = context.Background()
+var stream = "moonmoon"
 
 type StreamMessage struct {
 	Message string
@@ -18,53 +17,42 @@ type StreamMessage struct {
 
 func main() {
 	client := twitch.NewAnonymousClient() // for an anonymous user (no write capabilities)
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
+	// rdb := redis.NewClient(&redis.Options{
+	// 	Addr:     "localhost:6379",
+	// 	Password: "", // no password set
+	// 	DB:       0,  // use default DB
+	// })
+
+	rts := redisTS.NewClient("localhost:6379", "", nil)
 
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
 		for _, emote := range message.Emotes {
-			id, _ := uuid.NewRandom()
-			streamChannelName := fmt.Sprintf("%v_%v", message.Channel, strings.ToLower(emote.Name))
-
-			// add emote to stream without consumer
-			err := rdb.XAdd(ctx, &redis.XAddArgs{
-				Stream: streamChannelName,
-				MaxLen: 5,
-				Approx: false,
-				ID:     "",
-				Values: []interface{}{"message", fmt.Sprintf("%v %v %v - %v", message.Time.String(), emote.Name, emote.Count, id)},
-			}).Err()
-			if err != nil {
-				panic(err)
+			extension := "twitch"
+			options := redisTS.DefaultCreateOptions
+			labels := map[string]string{
+				"emote":     emote.Name,
+				"stream":    message.Channel,
+				"extension": "twitch",
 			}
+			options.Labels = labels
+
+			fmt.Println(labels)
+
+			key := fmt.Sprintf("%v/%v/%v", message.Channel, extension, emote.Name)
+
+			rts.CreateKeyWithOptions(key, options)
+
+			rts.AddWithOptions(key, message.Time.UnixMilli(), 1, options)
 
 			// examples of how to use XLEN
 			// fmt.Printf("the length is : %v\n", rdb.XLen(ctx, "swolenesss_kappa").Val())
 			// fmt.Printf("the length is : %v\n", rdb.XLen(ctx, "swolenesss_pogchamp").Val())
 
-			// publish message to channel that has a subscriber attached will automatically be consumed
-			err = rdb.Publish(ctx, streamChannelName, fmt.Sprintf("%v %v %v - %v", message.Time.String(), emote.Name, emote.Count, id)).Err()
-			if err != nil {
-				panic(err)
-			}
 		}
 	})
 
-	client.Join("swolenesss")
-	go connectClient(client)
-	// There is no error because go-redis automatically reconnects on error.
-	pubsub := rdb.Subscribe(ctx, "swolenesss_kappa")
-	// Close the subscription when we are done.
-	defer pubsub.Close()
-	ch := pubsub.Channel()
-
-	for msg := range ch {
-		fmt.Println(msg.Payload)
-	}
-
+	client.Join(stream)
+	connectClient(client)
 }
 
 func connectClient(client *twitch.Client) {
