1c43821c2546f5824e2afb0a9236b8d3e983b266
Forrest Miller <forrestmillerj@gmail.com>
2022-12-30 14:31:24 -0600

    lambda arn

--- infra/modules/aws/lambda/main.tf
+++ infra/modules/aws/lambda/main.tf
@@ -1,7 +1,7 @@
 module "lambda_hello_world" {
  source = "git::https://github.com/Fomiller/tf-module-lambda.git" 
  lambda_name    = var.lambda_name
- lambda_role    = var.lambda_role
+ lambda_role    = data.aws_iam_role.hello_world.arn
  filename         = "${path.module}/lambda_function.zip" 
  handler          = "lambda-go"
  source_code_hash = data.archive_file.zip.output_base64sha256
@@ -10,3 +10,6 @@ module "lambda_hello_world" {
  timeout          = 10
 }
 
+data "aws_iam_role" "hello_world" {
+    name = var.lambda_role
+}
