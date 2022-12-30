d721547ef5bc3cbff218b704b7aff1fbeec7936f
Forrest Miller <forrestmillerj@gmail.com>
2022-12-30 14:45:50 -0600

    lambda handler

--- infra/modules/aws/lambda/main.tf
+++ infra/modules/aws/lambda/main.tf
@@ -3,7 +3,7 @@ module "lambda_hello_world" {
  lambda_name    = var.lambda_name
  lambda_role    = data.aws_iam_role.hello_world.arn
  filename         = "${path.module}/lambda_function.zip" 
- handler          = "lambda-hello.exe"
+ handler          = "lambda_hello.exe"
  source_code_hash = data.archive_file.zip.output_base64sha256
  runtime          = "go1.x"
  memory_size      = 128
