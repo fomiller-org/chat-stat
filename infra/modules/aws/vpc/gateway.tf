26ba1f23b564c1ea59ef60be8407d4668913122d
Forrest Miller <forrestmillerj@gmail.com>
2023-01-02 15:02:17 -0600

    internet gw and route table

---
+++ infra/modules/aws/vpc/gateway.tf
@@ -0,0 +1,6 @@
+resource "aws_internet_gateway" "chat_stat" {
+    vpc_id = aws_vpc.chat_stat_main.id
+    tags = {
+        Name = "Chat Stat VPC IG"
+    }
+}
--- infra/modules/aws/vpc/subnets.tf
+++ infra/modules/aws/vpc/subnets.tf
@@ -17,3 +17,16 @@ resource "aws_subnet" "private_subnets" {
         Name = "Private Subnet ${count.index + 1}"
     }
 }
+
+resource "aws_route_table" "chat_stat_rt" {
+    vpc_id = aws_vpc.chat_stat_main.id
+    
+    route = {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.cha_stat_id
+    }
+    
+    tags = {
+        Name = "Chat Stat route table"
+    }
+}
