fb3ebdfe0bb294cf1b34c33bed4c041ae2cc1994
Forrest Miller <forrestmillerj@gmail.com>
2023-01-02 14:50:34 -0600

    create subnets

--- infra/modules/aws/vpc/subnets.tf
+++ infra/modules/aws/vpc/subnets.tf
@@ -1,19 +1,19 @@
-# resource "aws_subnet" "public_subnets" {
-#     count = length(var.public_subnet_cidrs)
-#     vpc_id = aws_vpc.chat_stat_main.id
-#     cidr_block = element(var.public_subnet_cidrs, count.index)
-#     availability_zone = element(var.azs, count.index)
-#     tags = {
-#         Name = "Public Subnet ${count.index + 1}"
-#     }
-# }
-#
-# resource "aws_subnet" "private_subnets" {
-#     count = length(var.private_subnet_cidrs)
-#     vpc_id = aws_vpc.chat_stat_main.id
-#     cidr_block = element(var.private_subnet_cidrs, count.index)
-#     availability_zone = element(var.azs, count.index)
-#     tags = {
-#         Name = "Private Subnet ${count.index + 1}"
-#     }
-# }
+resource "aws_subnet" "public_subnets" {
+    count = length(var.public_subnet_cidrs)
+    vpc_id = aws_vpc.chat_stat_main.id
+    cidr_block = element(var.public_subnet_cidrs, count.index)
+    availability_zone = element(var.azs, count.index)
+    tags = {
+        Name = "Public Subnet ${count.index + 1}"
+    }
+}
+
+resource "aws_subnet" "private_subnets" {
+    count = length(var.private_subnet_cidrs)
+    vpc_id = aws_vpc.chat_stat_main.id
+    cidr_block = element(var.private_subnet_cidrs, count.index)
+    availability_zone = element(var.azs, count.index)
+    tags = {
+        Name = "Private Subnet ${count.index + 1}"
+    }
+}
