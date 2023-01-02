716b93e96704f779f0e49a680475c51da4ec5b2b
Forrest Miller <forrestmillerj@gmail.com>
2023-01-02 14:32:41 -0600

    subnets init

---
+++ infra/modules/aws/vpc/subnets.tf
@@ -0,0 +1,17 @@
+resource "aws_subnet" "public_subnets" {
+    count = length(var.public_subnet_cidrs)
+    vpc_id = aws_vpc.chat_stat_main.id
+    cidr_block = element(var.public_subnet_cidrs, count.index)
+    tags = {
+        Name = "Public Subnet ${count.index + 1}"
+    }
+}
+
+resource "aws_subnet" "private_subnets" {
+    count = length(var.private_subnet_cidrs)
+    vpc_id = aws_vpc.chat_stat_main.id
+    cidr_block = element(var.private_subnet_cidrs, count.index)
+    tags = {
+        Name = "Private Subnet ${count.index + 1}"
+    }
+}
