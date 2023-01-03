6bdc9cd446dba0633b8ef6d1dbde28342f9584f5
Forrest Miller <forrestmillerj@gmail.com>
2023-01-03 11:40:45 -0600

    edit

--- src/cmd/lambda/twitch/webhookCallback/main.go
+++ src/cmd/lambda/twitch/webhookCallback/main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"context"
-	"log"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/nicklaw5/helix/v2"
 )
 
 type Event struct {
@@ -17,18 +16,19 @@ func main() {
 }
 
 func HandleRequest(ctx context.Context, event Event) {
-	webhookTopics := helix.GetWebhookTopicValuesFromRequest(event)
-	// handle events
-	if webhookTopics == helix.EventSubStreamOnlineEvent {
-		// create bot and start logging chat
-		// store bot id in dynamodb?
-	} else if event == helix.EventSubStreamOfflineEvent {
-		// disconnect bot and proccess chat data
-		// kick off step function?
-		// remove bot id from dynamodb?
-		// cleanup redis timeseries data?
-	} else {
-		// event not supported
-		log.Println("event not supported. Supported events EventSubStreamOfflineEvent, EventSubStreamOnlineEvent")
-	}
+	fmt.Println("webhook callback")
+	// webhookTopics := helix.GetWebhookTopicValuesFromRequest(event)
+	// // handle events
+	// if webhookTopics == helix.EventSubStreamOnlineEvent {
+	// 	// create bot and start logging chat
+	// 	// store bot id in dynamodb?
+	// } else if event == helix.EventSubStreamOfflineEvent {
+	// 	// disconnect bot and proccess chat data
+	// 	// kick off step function?
+	// 	// remove bot id from dynamodb?
+	// 	// cleanup redis timeseries data?
+	// } else {
+	// 	// event not supported
+	// 	log.Println("event not supported. Supported events EventSubStreamOfflineEvent, EventSubStreamOnlineEvent")
+	// }
 }
