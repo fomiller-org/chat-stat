228d9272eee02aa3dc5aa8800820d26da4d8277b
Forrest Miller <forrestmillerj@gmail.com>
2023-01-22 14:54:03 -0600

    update

--- login.sh
+++ login.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 apt-get update
-apt-get install git
+apt-get -y install git
 
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile saml
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile saml
+
+tgswitch 0.42.8
+
+echo "login complete"
 
