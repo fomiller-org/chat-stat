b89f674bf6eb2e3c023d92718cb51551e8c38b93
Forrest Miller <forrestmillerj@gmail.com>
2023-02-19 18:24:26 -0600

    init eventbridge; ecs event rule and target

---
+++ infra/modules/aws/eventbridge/rules.tf
@@ -0,0 +1,33 @@
+resource "aws_cloudwatch_event_rule" "cs_api_ecr_rule" {
+  name        = "${var.app_prefix}-ecr-api"
+  description = "Rule for SUCCESSFUL Push to chat stat api repo"
+
+  event_pattern = <<EOF
+{
+  "source": ["aws.ecr"],
+  "detail-type": ["ECR Image Action"],
+  "detail": {
+    "action-type": ["PUSH"],
+    "result": ["SUCCESS"],
+    "repository-name": ["695434033664.dkr.ecr.us-east-1.amazonaws.com/fomiller-chat-stat-api"]
+  }
+}
+EOF
+}
+
+# resource "aws_cloudwatch_event_rule" "cs_bot_ecr_rule" {
+#   name        = "${var.app_prefix}-ecr-api"
+#   description = "Rule for SUCCESSFUL Push to chat stat api repo"
+#
+#   event_pattern = <<EOF
+# {
+#   "source": ["aws.ecr"],
+#   "detail-type": ["ECR Image Action"],
+#   "detail": {
+#     "action-type": ["PUSH"],
+#     "result": ["SUCCESS"],
+#     "repository-name": ["695434033664.dkr.ecr.us-east-1.amazonaws.com/fomiller-chat-stat-bot"]
+#   }
+# }
+# EOF
+# }
---
+++ infra/modules/aws/eventbridge/targets.tf
@@ -0,0 +1,20 @@
+resource "aws_cloudwatch_event_target" "cs_api_ecr_target" {
+  target_id = "${var.app_prefix}-api-ecs"
+  arn       = var.cs_ecs_cluster_arn
+  rule      = aws_cloudwatch_event_rule.cs_api_ecr_rule.name
+
+  ecs_target {
+    task_count          = 1
+    task_definition_arn = var.cs_api_task_def_arn
+  }
+}
+
+# resource "aws_cloudwatch_event_target" "cs_bot_ecr_target" {
+#   target_id = "${var.app_prefix}-bot-ecs"
+#   arn       = var.cs_ecs_cluster_arn
+#
+#   ecs_target {
+#     task_count          = 1
+#     task_definition_arn = var.cs_bot__task_def_arn
+#   }
+# }
