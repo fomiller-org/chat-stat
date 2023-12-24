4caf598922d96628b05fb730018d3f7b60fc5f4d
Forrest Miller <forrestmillerj@gmail.com>
2023-12-24 15:22:25 -0600

    comment out failing cloudwatch event target

--- infra/modules/aws/eventbridge/targets.tf
+++ infra/modules/aws/eventbridge/targets.tf
@@ -1,14 +1,14 @@
-resource "aws_cloudwatch_event_target" "cs_api_ecr_target" {
-  target_id = "${var.namespace}-${var.app_prefix}-api-ecs"
-  arn       = var.cs_ecs_cluster_arn
-  rule      = aws_cloudwatch_event_rule.cs_api_ecr_rule.name
-  role_arn  = data.aws_iam_role.eventbridge_ecs_role.arn
-
-  ecs_target {
-    task_count          = 1
-    task_definition_arn = var.cs_api_task_def_arn
-  }
-}
+# resource "aws_cloudwatch_event_target" "cs_api_ecr_target" {
+#   target_id = "${var.namespace}-${var.app_prefix}-api-ecs"
+#   arn       = var.cs_ecs_cluster_arn
+#   rule      = aws_cloudwatch_event_rule.cs_api_ecr_rule.name
+#   role_arn  = data.aws_iam_role.eventbridge_ecs_role.arn
+#
+#   ecs_target {
+#     task_count          = 1
+#     task_definition_arn = var.cs_api_task_def_arn
+#   }
+# }
 
 # resource "aws_cloudwatch_event_target" "cs_bot_ecr_target" {
 #   target_id = "${var.app_prefix}-bot-ecs"
