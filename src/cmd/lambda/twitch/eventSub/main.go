42efb367e94403411cb8b8b0d9e29b840a2a3624
Forrest Miller <forrestmillerj@gmail.com>
2023-12-31 18:35:34 -0600

    change struct types

--- src/cmd/lambda/twitch/eventSub/main.go
+++ src/cmd/lambda/twitch/eventSub/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	// "github.com/aws/aws-sdk-go-v2/feature/dynamodbstreams/attributevalue"
 )
 
@@ -17,7 +19,7 @@ type DynamoDBEvent struct {
 // DynamoDBItem represents the structure of a DynamoDB item
 type DynamoDBItem struct {
 	// Define fields based on your DynamoDB table structure
-	StreamID string `dynamodbav:"StreamID" json:"StreamID"`
+	StreamID types.AttributeValueMemberS `dynamodbav:"StreamID" json:"StreamID"`
 	// Online   bool   `dynamodbav:"Online" json:"Online"`
 	// Add other fields as needed
 }
@@ -33,15 +35,21 @@ func HandleRequest(ctx context.Context, event DynamoDBEvent) {
 		fmt.Printf("Record Change: %v\n", record.Change)
 		fmt.Printf("Record Change NewImage: %v\n", record.Change.NewImage)
 		fmt.Printf("StreamID: %v\n", record.Change.NewImage["StreamID"])
-
-		recordData, err := json.Marshal(record.Change.NewImage)
+		// var myItem2 DynamoDBItem
+		// err := attributevalue.Unmarshal(record.Change.NewImage, &myItem2)
+		// if err != nil {
+		// 	fmt.Printf("Error UnMarshaling DynamoDBItem: %s", err)
+		// }
+		// fmt.Printf("Item: %v\n", myItem2)
+		//
+		recordData, err := attributevalue.Marshal(record.Change.NewImage)
 		if err != nil {
 			fmt.Printf("Error Marshaling recordData: %s", err)
 		}
 		fmt.Printf("Record Data: %v\n", recordData)
 
 		var myItem DynamoDBItem
-		err = json.Unmarshal(recordData, &myItem)
+		err = attributevalue.Unmarshal(recordData, &myItem)
 		if err != nil {
 			fmt.Printf("Error UnMarshaling DynamoDBItem: %s", err)
 		}
