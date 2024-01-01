bcb75f84d6035be79c7a1b9ebd7916aaa49f9364
Forrest Miller <forrestmillerj@gmail.com>
2024-01-01 11:02:39 -0600

    using gist

--- src/cmd/lambda/twitch/eventSub/main.go
+++ src/cmd/lambda/twitch/eventSub/main.go
@@ -9,10 +9,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodbstreams/attributevalue"
 )
 
-type DynamoDBEvent struct {
-	Records []events.DynamoDBStreamRecord `json:"Records"`
-}
-
 // MyDBItem represents the structure of a DynamoDB item
 type MyDBItem struct {
 	// Define fields based on your DynamoDB table structure
@@ -25,27 +21,27 @@ func main() {
 	lambda.Start(HandleRequest)
 }
 
-func HandleRequest(ctx context.Context, event DynamoDBEvent) {
+func HandleRequest(ctx context.Context, event events.DynamoDBEvent) {
 	fmt.Printf("Event: %v\n", event)
 	for _, record := range event.Records {
 		fmt.Printf("Record: %v\n", record)
-		fmt.Printf("Record Dynamodb: %v\n", record.Dynamodb.NewImage)
-		fmt.Printf("Record Dynamodb NewImage: %v\n", record.Dynamodb.NewImage)
-		fmt.Printf("StreamID: %v\n", record.Dynamodb.NewImage["StreamID"])
+		fmt.Printf("Record Change: %v\n", record.Change.NewImage)
+		fmt.Printf("Record Change NewImage: %v\n", record.Change.NewImage)
+		fmt.Printf("StreamID: %v\n", record.Change.NewImage["StreamID"])
 		// var myItem2 MyDBItem
-		// err := attributevalue.Unmarshal(record.Dynamodb.NewImage, &myItem2)
+		// err := attributevalue.Unmarshal(record.Change.NewImage, &myItem2)
 		// if err != nil {
 		// 	fmt.Printf("Error UnMarshaling MyDBItem: %s", err)
 		// }
 		// fmt.Printf("Item: %v\n", myItem2)
 		//
-		recordData, err := attributevalue.Marshal(record.Dynamodb.NewImage)
+		recordData, err := FromDynamoDBEventAVMap(record.Change.NewImage)
 		if err != nil {
 			fmt.Printf("Error Marshaling recordData: %s", err)
 		}
 		fmt.Printf("Record Data: %v\n", recordData)
 		var myItem MyDBItem
-		err = attributevalue.Unmarshal(recordData, &myItem)
+		err = attributevalue.UnmarshalMap(recordData, &myItem)
 		if err != nil {
 			fmt.Printf("Error UnMarshaling MyDBItem: %s", err)
 		}
