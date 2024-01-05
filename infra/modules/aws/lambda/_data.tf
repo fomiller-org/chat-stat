8d74559696ad99d204236d9945aa873975fe8b30
Forrest Miller <forrestmillerj@gmail.com>
2024-01-04 19:58:35 -0600

    refactor rust lambda dir

--- infra/modules/aws/lambda/_data.tf
+++ infra/modules/aws/lambda/_data.tf
@@ -6,6 +6,6 @@ data "archive_file" "hello_world" {
 
 data "archive_file" "event_sub" {
   type        = "zip"
-  source_file = "${path.module}/bin/twitch/eventSub/bootstrap"
-  output_path = "${path.module}/bin/twitch/eventSub/lambda_function.zip"
+  source_file = "${path.module}/bin/event-sub/bootstrap"
+  output_path = "${path.module}/bin/event-sub/lambda_function.zip"
 }
