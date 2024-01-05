4912def7487a2a7307247d00983f2c6b582529e4
Forrest Miller <forrestmillerj@gmail.com>
2024-01-05 16:09:41 -0600

    getting user id and readying for subscription event

--- src/lambdas/twitcheventsub/src/main.rs
+++ src/lambdas/twitcheventsub/src/main.rs
@@ -1,10 +1,12 @@
-// use std::fmt::{Display, Result};
-use std::fmt;
-use std::str::FromStr;
-
 use aws_lambda_events::{dynamodb::EventRecord, event::dynamodb::Event};
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 use serde::{Deserialize, Serialize};
+use std::env;
+use std::fmt;
+use std::str::FromStr;
+use twitch_api::eventsub;
+use twitch_api::twitch_oauth2::AppAccessToken;
+use twitch_api::HelixClient;
 
 #[derive(Debug, PartialEq)]
 enum EventName {
@@ -34,7 +36,7 @@ impl fmt::Display for EventName {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename = "StreamID")]
 struct MyModel {
     #[serde(rename = "StreamID")]
@@ -50,13 +52,13 @@ impl MyModel {
 }
 
 async fn function_handler(event: LambdaEvent<Event>) -> Result<(), Error> {
-    println!("Event: {:?}", event);
+    println!("Event: {:?}\n", event);
     for record in event.payload.records.iter() {
         let event_name = EventName::from_str(record.event_name.as_str()).unwrap();
         match event_name {
             EventName::Insert => {
                 println!("Opertaion type {}", event_name);
-                handle_insert(record).unwrap()
+                handle_insert(record).await?
             }
             EventName::Modify => {
                 println!("Opertaion type {}", event_name);
@@ -84,12 +86,32 @@ async fn main() -> Result<(), Error> {
     run(service_fn(function_handler)).await
 }
 
-fn handle_insert(record: &EventRecord) -> Result<(), Error> {
+async fn handle_insert(record: &EventRecord) -> Result<(), Error> {
     let new_image = record.change.new_image.clone();
     let new_item: MyModel = serde_dynamo::from_item(new_image)?;
 
-    println!("NEW StreamID: {:?}", new_item.stream_id);
-    println!("NEW Online Status: {:?}", new_item.get_online());
+    println!("NEW StreamID: {:?}", &new_item.stream_id);
+    println!("NEW Online Status: {:?}", &new_item.clone().get_online());
+
+    let twitch_client_secret: String = env::var("TWITCH_CLIENT_SECRET").unwrap();
+    let twitch_client_id: String = env::var("TWITCH_CLIENT_ID").unwrap();
+
+    let helix: HelixClient<reqwest::Client> = HelixClient::new();
+    let token = AppAccessToken::get_app_access_token(
+        &helix,
+        twitch_client_id.into(),
+        twitch_client_secret.into(),
+        vec![],
+    )
+    .await?;
+
+    let user = helix
+        .get_user_from_login(&new_item.stream_id, &token)
+        .await?
+        .unwrap();
+    let event = eventsub::stream::online::StreamOnlineV1::broadcaster_user_id(user.id);
+
+    println!("{:?}", event);
 
     Ok(())
 }
