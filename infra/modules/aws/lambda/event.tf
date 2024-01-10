14d52d1328a4b23caba43283a0cce83644eb1407
Forrest Miller <forrestmillerj@gmail.com>
2024-01-09 20:59:08 -0600

    filtering dynamodb event for lambda trigger

--- infra/modules/aws/lambda/event.tf
+++ infra/modules/aws/lambda/event.tf
@@ -3,4 +3,11 @@ resource "aws_lambda_event_source_mapping" "event_sub" {
   function_name     = aws_lambda_function.twitch_event_sub.function_name
   starting_position = "LATEST"
   batch_size        = 1
+  filter_criteria {
+    filter {
+      pattern = jsonencode({
+        eventName = ["INSERT", "REMOVE"]
+      })
+    }
+  }
 }
