4c18f5a4d1ccd23c813b2c1fb251277dacfa845a
Forrest Miller <forrestmillerj@gmail.com>
2024-01-09 21:52:07 -0600

    iam perms dynamodb

--- infra/modules/aws/iam/policies/policy_twitch_event_sub.tf
+++ infra/modules/aws/iam/policies/policy_twitch_event_sub.tf
@@ -13,6 +13,26 @@ data "aws_iam_policy_document" "lambda_event_sub" {
       "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${var.namespace}-${var.app_prefix}/stream/*"
     ]
   }
+
+  statement {
+    sid    = "LambdaDynamoDBTablePermissions"
+    effect = "Allow"
+    actions = [
+      "dynamodb:BatchGetItem",
+      "dynamodb:BatchWriteItem",
+      "dynamodb:ConditionCheckItem",
+      "dynamodb:PutItem",
+      "dynamodb:DescribeTable",
+      "dynamodb:DeleteItem",
+      "dynamodb:GetItem",
+      "dynamodb:Scan",
+      "dynamodb:Query",
+      "dynamodb:UpdateItem"
+    ]
+    resources = [
+      "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${var.namespace}-${var.app_prefix}"
+    ]
+  }
 }
 
 resource "aws_iam_policy" "lambda_event_sub" {
