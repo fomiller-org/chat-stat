c2a9e0601e6c33e7658ec2c72c7b06b9523a3b93
Forrest Miller <forrestmillerj@gmail.com>
2024-01-11 20:24:42 -0600

    log groups

--- infra/modules/aws/lambda/cloudwatch.tf
+++ infra/modules/aws/lambda/cloudwatch.tf
@@ -8,7 +8,13 @@ resource "aws_cloudwatch_log_group" "event_sub" {
   retention_in_days = 7
 }
 
-resource "aws_cloudwatch_log_group" "event_sub" {
+resource "aws_cloudwatch_log_group" "twitch_event_sub_webhook" {
+  name              = "/aws/lambda/${aws_lambda_function.twitch_event_sub_webhook.function_name}"
+  retention_in_days = 7
+}
+
+resource "aws_cloudwatch_log_group" "twitch_record_manager" {
   name              = "/aws/lambda/${aws_lambda_function.twitch_event_record_manager.function_name}"
   retention_in_days = 7
 }
+
