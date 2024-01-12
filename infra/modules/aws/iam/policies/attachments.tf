83f57debb0274935acd6bed9c4e428de1e01cac2
Forrest Miller <forrestmillerj@gmail.com>
2024-01-11 20:59:18 -0600

    record manager basic policy

--- infra/modules/aws/iam/policies/attachments.tf
+++ infra/modules/aws/iam/policies/attachments.tf
@@ -8,6 +8,11 @@ resource "aws_iam_role_policy_attachment" "lambda_twitch_event_sub_webhook_basic
   role       = var.iam_role_name_lambda_twitch_event_sub_webhook
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_twitch_record_manager_basic_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = var.iam_role_name_lambda_twitch_record_manager
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_twitch_event_sub_dynamodb_attachment" {
   policy_arn = aws_iam_policy.lambda_event_sub.arn
   role       = var.iam_role_name_lambda_twitch_event_sub
