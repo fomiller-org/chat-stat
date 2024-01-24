1a5b0989d2fde21bb6a33daae356f415436bc535
Forrest Miller <forrestmillerj@gmail.com>
2024-01-24 07:10:53 -0600

    path params

--- src/api/src/main.rs
+++ src/api/src/main.rs
@@ -1,9 +1,16 @@
-use axum::{response::Html, routing::*, Router};
+use axum::{
+    extract::{Json, Path, Query},
+    response::Html,
+    routing::*,
+    Router,
+};
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .route("/:id", get(my_post));
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -13,7 +20,11 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello!</h1>")
+async fn hello_world() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
 
+async fn my_post(Path(user_id): Path<String>) -> Html<String> {
+    let res = format!("<h1>Hello, user {}!</h1>", user_id);
+    Html(res)
+}
