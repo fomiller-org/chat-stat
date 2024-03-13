fa0521bea44231b56c6c77b211bfa74a4dd14688
Forrest Miller <forrestmillerj@gmail.com>
2024-03-12 20:48:54 -0500

    adding apexCharts to svelte

---
+++ src/frontend/src/routes/dashboard/+page.svelte
@@ -0,0 +1,94 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+        <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+</svelte:head>
+
+<div class="text-column">
+	<h1 class="text-3xl font-bold underline ">Dashboard</h1>
+
+	<p>
+		My dashboard page
+	</p>
+    <Chart/>
+</div>
+
+<script>
+    import Chart from '../Chart.svelte';
+    import { browser } from '$app/environment';
+
+const options = {
+    chart: {
+        height: "100%",
+        maxWidth: "100%",
+        type: "area",
+        fontFamily: "Inter, sans-serif",
+        dropShadow: {
+            enabled: false,
+        },
+        toolbar: {
+            show: false,
+        },
+    },
+    tooltip: {
+        enabled: true,
+        x: {
+            show: false,
+        },
+    },
+    fill: {
+        type: "gradient",
+        gradient: {
+            opacityFrom: 0.55,
+            opacityTo: 0,
+            shade: "#1C64F2",
+            gradientToColors: ["#1C64F2"],
+        },
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    stroke: {
+        width: 6,
+    },
+    grid: {
+        show: false,
+        strokeDashArray: 4,
+        padding: {
+            left: 2,
+            right: 2,
+            top: 0
+        },
+    },
+    series: [
+        {
+            name: "New users",
+            data: [6500, 6418, 6456, 6526, 6356, 6456],
+            color: "#1A56DB",
+        },
+    ],
+    xaxis: {
+        categories: ['01 February', '02 February', '03 February', '04 February', '05 February', '06 February', '07 February'],
+        labels: {
+            show: false,
+        },
+        axisBorder: {
+            show: false,
+        },
+        axisTicks: {
+            show: false,
+        },
+    },
+    yaxis: {
+        show: false,
+    },
+}
+
+if (browser) {
+    if (document.getElementById("area-chart") && typeof ApexCharts !== 'undefined') {
+        const chart = new ApexCharts(document.getElementById("area-chart"), options);
+        console.log(options);
+        chart.render();
+    }
+}
+</script>
