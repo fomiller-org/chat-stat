fa077948289378605b96825aa06897cab6ce89e3
Forrest Miller <forrestmillerj@gmail.com>
2024-04-12 13:17:00 -0500

    remove hello world lambda

--- infra/modules/aws/lambda/lambda.tf
+++ infra/modules/aws/lambda/lambda.tf
@@ -1,20 +1,3 @@
-resource "aws_lambda_function" "hello_world" {
-  function_name    = "${var.namespace}-${var.app_prefix}-${var.lambda_name}"
-  role             = data.aws_iam_role.hello_world.arn
-  handler          = "bootstrap"
-  filename         = "${path.module}/bin/hello/lambda_function.zip"
-  source_code_hash = data.archive_file.hello_world.output_base64sha256
-  runtime          = "provided.al2"
-  architectures    = ["arm64"]
-  memory_size      = 128
-  timeout          = 10
-}
-
-data "aws_iam_role" "hello_world" {
-  name = var.lambda_role
-}
-
-
 resource "aws_lambda_function" "twitch_event_sub" {
   function_name    = "${var.namespace}-${var.app_prefix}-twitch-event-sub"
   role             = var.iam_role_arn_lambda_twitch_event_sub
