bfc69c71872d33e1a9f915abd541cacc94ef1e76
Forrest Miller <forrestmillerj@gmail.com>
2024-04-19 15:48:18 -0500

    unloading channel and cross channel top moment queries to s3 with lambda

--- src/lambdas/timestreamquery/src/main.rs
+++ src/lambdas/timestreamquery/src/main.rs
@@ -1,6 +1,8 @@
 use aws_config::BehaviorVersion;
+use chrono::{DateTime, Datelike, TimeDelta, Utc};
 use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
 use serde::{Deserialize, Serialize};
+use std::env;
 
 use strum_macros::EnumString;
 
@@ -11,6 +13,7 @@ struct Request {
     time: Option<String>,
     limit: Option<String>,
     bin_time: Option<String>,
+    start_time: Option<String>,
 }
 
 #[derive(Serialize)]
@@ -44,11 +47,15 @@ enum QuerySchedule {
 enum TimestreamQuery {
     TopMomentsByChannel(String),
     TopMomentsCrossChannel(String),
+    UnloadTopMomentsCrossChannel(String),
 }
 
 async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
     // Extract some useful info from the request
-    println!("{:?}", event.payload);
+    let chat_stat_bucket = env::var("CHAT_STAT_BUCKET")?;
+    let chat_stat_prefix = "timestream";
+    let dates = get_date();
+
     let config = aws_config::defaults(BehaviorVersion::latest())
         .region("us-east-1")
         .load()
@@ -87,6 +94,76 @@ async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error
                 binned_timestamp, time, binned_timestamp, limit,
             )))
         }
+        "unload_top_moments_cross_channel" => {
+            let binned_timestamp = event.payload.bin_time.expect("missing channel for query");
+            let time = event.payload.time.expect("missing time for query");
+            let limit = event.payload.limit.expect("missing limit for query");
+            Ok(TimestreamQuery::UnloadTopMomentsCrossChannel(format!(
+                r#"
+                UNLOAD(
+                SELECT BIN(time, {}) AS binned_timestamp, count(emote) AS emote_count, emote, channel
+                FROM fomiller.chat_stat
+                WHERE measure_name = 'count'
+                    AND time > ago({})
+                GROUP BY BIN(time, {}), emote, channel
+                ORDER BY emote_count DESC LIMIT {}
+                )
+                TO 's3://{}/{}/year={}/month={:0>2}/day={}/'
+                WITH (
+                    format='CSV',
+                    compression='NONE'
+                )
+                "#,
+                binned_timestamp,
+                time,
+                binned_timestamp,
+                limit,
+                chat_stat_bucket,
+                chat_stat_prefix,
+                dates.0,
+                dates.1,
+                dates.2
+            )))
+        }
+        "unload_top_moments_by_channel" => {
+            let channel = event.payload.channel.expect("missing channel");
+            let binned_timestamp = event.payload.bin_time.expect("missing channel for query");
+            let limit = event.payload.limit.expect("missing limit for query");
+            let start = DateTime::parse_from_rfc3339(
+                &event.payload.start_time.expect("missing start time"),
+            )?
+            .to_utc();
+            let elapsed_time = (Utc::now() - start).num_minutes();
+            Ok(TimestreamQuery::TopMomentsByChannel(format!(
+                r#"
+                UNLOAD(
+                SELECT BIN(time, {}) AS binned_timestamp, count(emote) AS emote_count, emote, channel
+                FROM fomiller.chat_stat
+                WHERE measure_name = 'count'
+                    AND time > ago({}m)
+                    AND channel = '{}'
+                GROUP BY BIN(time, {}), emote, channel
+                ORDER BY emote_count DESC LIMIT {}
+                )
+                TO 's3://{}/{}/year={}/month={:0>2}/day={}/'
+                WITH (
+                    format='CSV',
+                    compression='NONE',
+                    partitioned_by= ARRAY ['channel']
+                )
+                "#,
+                binned_timestamp,
+                elapsed_time,
+                channel,
+                binned_timestamp,
+                limit,
+                chat_stat_bucket,
+                chat_stat_prefix,
+                dates.0,
+                dates.1,
+                dates.2
+            )))
+        }
         _ => Err(format!(
             "TimestreamQuery not supported: {}",
             event.payload.query_type
@@ -100,21 +177,26 @@ async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error
         TimestreamQuery::TopMomentsCrossChannel(q) => {
             query_client.0.query().query_string(q).send().await
         }
+        TimestreamQuery::UnloadTopMomentsCrossChannel(q) => {
+            println!("QUERY {}", q);
+            query_client.0.query().query_string(q).send().await
+        }
     };
 
     match res {
         Ok(r) => {
-            let x: Vec<_> = r
-                .rows()
-                .into_iter()
-                .map(|r| r.data().into_iter().map(|d| return d).collect::<Vec<_>>())
-                .collect();
-            for v in x {
-                println!("TIME: {:?}", v[0].scalar_value().unwrap());
-                println!("EMOTE_COUNT: {:?}", v[1].scalar_value().unwrap());
-                println!("EMOTE: {:?}", v[2].scalar_value().unwrap());
-                println!("CHANNEL: {:?}\n", v[3].scalar_value().unwrap())
-            }
+            println!("{:?}", r);
+            // let x: Vec<_> = r
+            //     .rows()
+            //     .into_iter()
+            //     .map(|r| r.data().into_iter().map(|d| return d).collect::<Vec<_>>())
+            //     .collect();
+            // for v in x {
+            //     println!("TIME: {:?}", v[0].scalar_value().unwrap());
+            //     println!("EMOTE_COUNT: {:?}", v[1].scalar_value().unwrap());
+            //     println!("EMOTE: {:?}", v[2].scalar_value().unwrap());
+            //     println!("CHANNEL: {:?}\n", v[3].scalar_value().unwrap())
+            // }
         }
         Err(e) => {
             eprintln!("{:?}", e.as_service_error());
@@ -122,7 +204,7 @@ async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error
     }
 
     let resp = Response {
-        msg: format!("success {}", "hellow"),
+        msg: "success".to_string(),
     };
 
     Ok(resp)
@@ -133,4 +215,9 @@ async fn main() -> Result<(), Error> {
     tracing::init_default_subscriber();
 
     run(service_fn(function_handler)).await
+}
+
+fn get_date() -> (i32, u32, u32) {
+    let now = Utc::now();
+    (now.year(), now.month(), now.day())
 }
